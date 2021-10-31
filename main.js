00d79773ca4386f14b9a0a41a13017d4e1bfc8de
franciscomarin98 <francmarinc@gmail.com>
2021-10-30 23:49:30 -0500

    :sparkles: Public, private and static methods.

--- main.js
+++ main.js
@@ -6,10 +6,18 @@
             this.team = team;
             this.realName = realName;
         }
+        Avenger.prototype.bio = function () {
+            return this.name + " (" + this.team + ")";
+        };
+        Avenger.avengerAge = function () {
+            return this.age;
+        };
         Avenger.age = 35;
         return Avenger;
     }());
     var antMan = new Avenger('AntMan', 'Team Cap', 'Scott Lang');
     console.log(antMan);
+    console.log(antMan.bio());
+    console.log(Avenger.avengerAge());
 })();
 //# sourceMappingURL=main.js.map
--- src/06_clases/basica/index.ts
+++ src/06_clases/basica/index.ts
@@ -2,27 +2,38 @@
 
     class Avenger {
 
-/*        private name: string;
-        private team: string;
-        public realName?: string;*/
+        /*        private name: string;
+                private team: string;
+                public realName?: string;*/
         static age: number = 35;
 
 
-/*        constructor(name: string, team: string, realName: string) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }*/
+        /*        constructor(name: string, team: string, realName: string) {
+                    this.name = name;
+                    this.team = team;
+                    this.realName = realName;
+                }*/
 
         constructor(
             private name: string,
             private team: string,
             public realName?: string
-        ) {}
+        ) {
+        }
+
+        public bio() {
+            return `${this.name} (${this.team})`
+        }
+
+        static avengerAge(){
+            return this.age;
+        }
 
     }
 
     const antMan: Avenger = new Avenger('AntMan', 'Team Cap', 'Scott Lang');
     console.log(antMan);
+    console.log(antMan.bio())
+    console.log(Avenger.avengerAge())
 
 })();
