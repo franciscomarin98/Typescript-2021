b561336f7a227b191569ca1d1584170496e71ec1
franciscomarin98 <francmarinc@gmail.com>
2021-11-08 11:35:11 -0500

    :zap: feat(namespaces):
    Exports and imports

---
+++ src/09_ts-node/src/data/powers.ts
@@ -0,0 +1,18 @@
+export interface Powers {
+    id: number;
+    desc: string;
+}
+
+
+const powers: Powers[] = [
+    {
+        id: 1,
+        desc: 'Money'
+    },
+    {
+        id: 2,
+        desc: 'thunder'
+    }
+]
+
+export default powers;
--- src/09_ts-node/src/index.ts
+++ src/09_ts-node/src/index.ts
@@ -1,7 +1,11 @@
+// import {Hero} from "./classes/Hero";
+// import {Hero as SuperHero} from "./classes/Hero";
+import * as HeroClasses from "./classes/Hero";
+import powers2 from "./data/powers";
 
-
-console.log('Hola Mundo!');
-
+const hero = new HeroClasses.Hero('Ironman', 1, 55);
+console.log(hero);
+console.log(powers2);
 
 
 
